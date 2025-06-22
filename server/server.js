28daa7d1db8d795551c2e7d67ad9aee2113877c6
Yash Verma <114978813+Rajput-xv@users.noreply.github.com>
2025-06-23 01:03:39 +0530

    update url

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
-    ? ['https://app-name.vercel.app'] 
+    ? ['https://generate-random-password-mu.vercel.app'] 
     : ['http://localhost:3000'],
   credentials: true
 }));
